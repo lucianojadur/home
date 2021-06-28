c625a4703492671076674fd74cc6cef7fb12859f
lucianojadur <ljadur@fi.uba.ar>
2021-06-28 17:22:07 -0300

    update javascript

--- changeElements.js
+++ changeElements.js
@@ -1,19 +1,23 @@
 
-const darkPath = /*'file:///home/luciano/Escritorio/frontend*/'dark.css';
-const lightPath = /*'file:///home/luciano/Escritorio/frontend*/'light.css';
+const darkPath = "dark.css";
+const lightPath = "light.css";
 
 
 function showText(id, text){
-    document.getElementById(id).innerHTML = text;
+	document.getElementById(id).innerHTML = text;
 }
 
 function flipTextContent(id1, id2){
-    let aux = document.getElementById(id1).innerHTML;
-    document.getElementById(id1).innerHTML = document.getElementById(id2).innerHTML;
-    document.getElementById(id2).innerHTML = aux;
+	let aux = document.getElementById(id1).innerHTML;
+	document.getElementById(id1).innerHTML = document.getElementById(id2).innerHTML;
+	document.getElementById(id2).innerHTML = aux;
 }
 
 function changeStyle(){
-    let link = document.getElementById('style')
-    link.href = (link.href === darkPath) ? link.href = lightPath : darkPath;
+	let link = document.getElementById('style');
+	if(link.href === lightPath) {
+		link.href = darkPath;
+	}else if (link.href === darkPath) {
+		link.href = lightPath;
+	}
 }
